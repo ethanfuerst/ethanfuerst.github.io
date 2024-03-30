82fb2728e161d5c079945818aa2e6707b11607da
Ethan Fuerst <ethanfuerst@gmail.com>
2024-03-30 13:52:01 -0400

    changing config

--- freeze.py
+++ freeze.py
@@ -1,7 +1,9 @@
 from flask_frozen import Freezer
 from app import app
 
+# maybe change
+app.config["FREEZER_DESTINATION"] = "build"
 freezer = Freezer(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     freezer.freeze()
