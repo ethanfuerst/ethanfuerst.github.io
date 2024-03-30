bb77944a57fe6ac771c48f9bf16e126edae5c2e3
Ethan Fuerst <ethanfuerst@gmail.com>
2024-03-30 13:55:19 -0400

    changing build location

--- freeze.py
+++ freeze.py
@@ -2,7 +2,7 @@ from flask_frozen import Freezer
 from app import app
 
 # maybe change
-app.config["FREEZER_DESTINATION"] = "build"
+app.config["FREEZER_DESTINATION"] = "../docs"
 freezer = Freezer(app)
 
 if __name__ == "__main__":
